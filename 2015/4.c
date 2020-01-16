3472e4c74abb31b60caa33cecf37ddebbf13a891
David Pflug <david@pflug.email>
2020-01-15 23:40:54 -0500

    Complete 2015 4-1 and 4-2

---
+++ 2015/4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+#include <limits.h>
+
+const char* string = "bgvyzdsv";
+const char* zeros = "00000";
+
+int main() {
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    char md5str[32];
+    int i = 0;
+
+    do {
+        ++i;
+        sprintf(md5str, "%s%i", string, i);
+        MD5((unsigned char*)md5str, strlen(md5str), digest);
+    } while (digest[0] != 0 || digest[1] != 0 || digest[2] > 15);
+
+    printf("Part 1: %i\n", i);
+
+    do {
+      ++i;
+        sprintf(md5str, "%s%i", string, i);
+        MD5((unsigned char*)md5str, strlen(md5str), digest);
+    } while (digest[0] != 0 || digest[1] != 0 || digest[2] != 0);
+
+    printf("Part 2: %i\n", i);
+}
