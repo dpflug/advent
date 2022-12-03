e03cbde4610218763712ba1ea9d5126e941c5bb1
David Pflug <david@pflug.io>
2022-12-03 16:14:37 -0500

    Complete 2022 2-2

--- 2022/2022.scm
+++ 2022/2022.scm
@@ -1,3 +1,6 @@
+;;  Advent of Code 2022
+;;
+
 (define (day1-1 elf-counts)
   (let ((count-list (string-split elf-counts #\Newline)))
     (let reduce ((to-count count-list) (max-elf 0) (current-elf 0))
@@ -27,9 +30,11 @@
   (define (parse-turn str)
     (list (string-ref str 0) (string-ref str 2)))
   (define (move->value char)
+    ;;  A & X = 1, B & Y = 2, C & Z = 3
     (- (modulo (char->integer char) 23) 18))
   (define (outcome turn)
-    (list-ref '(0 6 3 0 6)              ; 0: loss, 3: draw, 6: win
+    ;;  0 on loss, 3 on draw, 6 on win
+    (list-ref '(0 6 3 0 6)
               (+ 2 (- (car turn) (cadr turn)))))
   (define (sum-score to-count sum)
     (if (null? to-count)
@@ -38,6 +43,29 @@
                (this-turn (map move->value this-turn-strings))
                (my-move (cadr this-turn))
                (this-outcome (outcome this-turn)))
+          (sum-score (cdr to-count) (+ sum my-move this-outcome)))))
+  (let* ((turns (string-split input #\Newline))
+         (movelist (map parse-turn turns)))
+    (sum-score movelist 0)))
+
+(define (day2-2 input)
+  (define (parse-turn str)
+    (list (string-ref str 0) (string-ref str 2)))
+  (define (move->value char)
+    (- (modulo (char->integer char) 23) 18))
+  (define (outcome turn)
+    ;;  0 on loss, 3 on draw, 6 on win
+    (list-ref '(0 6 3 0 6)
+              (+ 2 (- (car turn) (cadr turn)))))
+  (define (pick-move turn)
+    (1+ (modulo (+ (car turn) (- (1- (cadr turn)) 2)) 3)))
+  (define (sum-score to-count sum)
+    (if (null? to-count)
+        sum
+        (let* ((this-turn-strings (car to-count))
+               (this-turn (map move->value this-turn-strings))
+               (my-move (pick-move this-turn))
+               (this-outcome (outcome (list (car this-turn) my-move))))
           (sum-score (cdr to-count) (+ sum my-move this-outcome)))))
   (let* ((turns (string-split input #\Newline))
          (movelist (map parse-turn turns)))
